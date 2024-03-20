d9d12f0e4abc0be565e80b0130aaa6c28ff5bea8
maretar473 <73227568+maretar473@users.noreply.github.com>
2024-03-20 14:20:57 +0100

    Add files via upload

---
+++ MT_TAIIB_WebApi/DataAccesLayer/Migrations/20240320124425_cw3.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class cw3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Product_Id",
+                table: "OrderPosition",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderPosition_Product_Id",
+                table: "OrderPosition",
+                column: "Product_Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderPosition_Product_Product_Id",
+                table: "OrderPosition",
+                column: "Product_Id",
+                principalTable: "Product",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderPosition_Product_Product_Id",
+                table: "OrderPosition");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderPosition_Product_Id",
+                table: "OrderPosition");
+
+            migrationBuilder.DropColumn(
+                name: "Product_Id",
+                table: "OrderPosition");
+        }
+    }
+}
